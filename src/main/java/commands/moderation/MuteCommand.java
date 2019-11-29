d7895b2016238cd4f0824d6b085a8f0513871bc3
zack <zackmyers@lavabit.com>
2019-11-28 23:02:25 -1000

    xd

--- src/main/java/commands/moderation/MuteCommand.java
+++ src/main/java/commands/moderation/MuteCommand.java
@@ -1,4 +1,20 @@
 package commands.moderation;
 
-public class MuteCommand {
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+import utils.MuteUtil;
+
+public class MuteCommand extends Command {
+    public MuteCommand() {
+        this.name = "mute";
+        this.help = "Mutes a user";
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        MuteUtil mUtil = new MuteUtil();
+        String[] args = event.getArgs().split(" ");
+        mUtil.mute(event, args[2], args[1]);
+    }
 }
