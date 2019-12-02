6fb606c929078666913d723149f4454c9f8698c5
zack <zackmyers@lavabit.com>
2019-12-01 19:24:41 -1000

    Add mute functionality

--- src/main/java/commands/moderation/MuteCommand.java
+++ src/main/java/commands/moderation/MuteCommand.java
@@ -3,18 +3,35 @@ package commands.moderation;
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 
+import net.dv8tion.jda.api.Permission;
 import utils.MuteUtil;
 
 public class MuteCommand extends Command {
+
     public MuteCommand() {
         this.name = "mute";
         this.help = "Mutes a user";
+        this.aliases = new String[]{"m"};
+        this.arguments = "[user] [time] [reason] - ALL REQUIRED!";
+        this.category = new Category("Moderation");
+        this.userPermissions = new Permission[]{Permission.KICK_MEMBERS};
     }
 
     @Override
     protected void execute(CommandEvent event) {
         MuteUtil mUtil = new MuteUtil();
         String[] args = event.getArgs().split(" ");
+
+        if (args[1].isEmpty()) {
+            event.getChannel().sendMessage("Please provide a time").queue();
+            return;
+        }
+
+        if (args[2].isEmpty()) {
+            event.getChannel().sendMessage("Please provide a reason").queue();
+        }
+
         mUtil.mute(event, args[2], args[1]);
     }
+
 }
--- src/main/java/utils/MuteUtil.java
+++ src/main/java/utils/MuteUtil.java
@@ -2,9 +2,8 @@ package utils;
 
 import java.awt.Color;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
 
@@ -13,6 +12,7 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.ReadyEvent;
 
 public class MuteUtil {
     public void mute(final CommandEvent event, String reason, final String time) {
@@ -49,21 +49,50 @@ public class MuteUtil {
             }
         }
 
-        final LocalDateTime currentTime = LocalDateTime.now();
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyyHH:mm:ss");
-
         try {
             final PreparedStatement statement = sqlUtils.connect()
-                    .prepareStatement("insert into mutes values (?, ?, ?, ?)");
+                    .prepareStatement("insert into mutes values (?, ?, ?, ?, ?)");
             statement.setString(1, member.getUser().getId());
-            statement.setString(2, reason);
-            statement.setString(3, currentTime.format(formatter));
-            statement.setString(4, currentTime.plusMinutes(Long.parseLong(time)).format(formatter));
+            statement.setString(2, guild.getId());
+            statement.setString(3, reason);
+            statement.setLong(4, System.currentTimeMillis());
+            statement.setLong(5, System.currentTimeMillis() + (60000 * Integer.parseInt(time)));
             statement.execute();
         } catch (final SQLException e) {
             e.printStackTrace();
         }
 
+        System.out.println("Muted " + member.getEffectiveName());
         sqlUtils.disconnect();
+    }
+
+    public void checkMutes(ReadyEvent event) throws SQLException {
+        System.out.println("Checking for people to unmute");
+        long time = System.currentTimeMillis();
+        MySQLUtils sqlUtils = new MySQLUtils();
+        PreparedStatement statement = sqlUtils.connect().prepareStatement("select * from mutes");
+        ResultSet set = statement.executeQuery();
+        while (set.next()) {
+            long umTime = set.getLong(5);
+            String id = set.getString(1);
+            String gID = set.getString(2);
+            long sTime = set.getLong(4);
+            if (time > umTime) {
+                Guild guild = event.getJDA().getGuildById(gID);
+                assert guild != null;
+                Member member = guild.getMemberById(id);
+                assert member != null;
+                unMute(member, guild);
+                PreparedStatement stmt = sqlUtils.connect().prepareStatement("delete from mutes where userID = " + id + " and currentTime = " + sTime + " limit 1");
+                stmt.execute();
+            }
+        }
+        sqlUtils.disconnect();
+    }
+
+    public void unMute(Member member, Guild guild) {
+        Role role = guild.getRolesByName("Artemis Muted", false).get(0);
+        guild.removeRoleFromMember(member, role).queue();
+        System.out.println("Unmuted " + member.getEffectiveName());
     }
 }
