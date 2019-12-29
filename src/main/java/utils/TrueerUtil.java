c344ede2f49fe31eec37fcfc1dbe6f05f37b0490
zack <zackmyers@lavabit.com>
2019-12-28 19:49:29 -1000

    check that the author isn't a bot
    (whoops)

--- src/main/java/utils/TrueerUtil.java
+++ src/main/java/utils/TrueerUtil.java
@@ -6,9 +6,11 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class TrueerUtil extends ListenerAdapter {
 
     public void onMessageReceived(MessageReceivedEvent event) {
-        if ((event.getGuild().getId().equals("566901455395618838")) || event.getGuild().getId().equals("609572230141182002")) {
-            if (event.getMessage().getContentRaw().toLowerCase().equals("true :lulw:")) {
-                event.getMessage().getChannel().sendMessage("TRUE <:LULW:660717129795895307>").queue();
+        if (!event.getMessage().getAuthor().isBot()) {
+            if ((event.getGuild().getId().equals("566901455395618838")) || event.getGuild().getId().equals("609572230141182002")) {
+                if (event.getMessage().getContentRaw().toLowerCase().equals("true :lulw:")) {
+                    event.getMessage().getChannel().sendMessage("TRUE <:LULW:660717129795895307>").queue();
+                }
             }
         }
     }
